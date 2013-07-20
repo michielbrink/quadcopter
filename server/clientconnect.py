df4357a4dbbed200b43ec32d3b75cc44fe2db0d1
michielbrink <michielpa@gmail.com>
2013-07-20 11:45:27 +0200

    modified:   clientconnect.py

--- server/clientconnect.py
+++ server/clientconnect.py
@@ -1,10 +1,10 @@
+import socket
 TCP_IP = ''
 TCP_PORT = 5555
 BUFFER_SIZE = 1024
 
 class client_device:
     def __init__(self):
-        import socket
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((TCP_IP, TCP_PORT))
         s.listen(1)
