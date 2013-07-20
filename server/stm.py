847aa12e73eb98bdd793535f1e1fea7e3a3715d1
michielbrink <michielpa@gmail.com>
2013-07-20 16:01:46 +0200

    modified:   stm.py

--- server/stm.py
+++ server/stm.py
@@ -30,22 +30,6 @@ class stm_message:
     def get_message(self):
         return self.msg
 
-    def __repr__(self):
-        return "--> %02X : %s : %02X" % (
-            self.msg[0],  ' '.join(["%02X" % x for x in self.msg[1:-2]]), self.msg[-1] )
-
-class stm_response:
-    def __init__(self, message):
-        print repr(message)
-        self.msg = message
-        cmd = message[0]
-        payload = message[1:PAYLOAD_LENGTH+1]
-        par = message[-1]
-        par_check = reduce(lambda x, y: x^y, message)
-    def __repr__(self):
-        return "<-- %02X : %s : %02X" % (
-            self.msg[0],  ' '.join(["%02X" % x for x in self.msg[1:-2]]), self.msg[-1] )
-
 # Class to interact with the QuadCopter-STM in a more sane way
 class stm_device:
     def __init__(self):
