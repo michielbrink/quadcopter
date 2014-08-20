3eaa186c7b6ba84e66801559f245f62dcae0a2f4
michielbrink <michielpa@gmail.com>
2014-08-20 15:03:28 +0200

    deleted:    client/client.py 	deleted:    client/clientemulator.py 	deleted:    client/start.sh 	deleted:    client/test/client.py 	deleted:    client/test/xbox_read.py 	deleted:    client/xcontroller.py

--- client/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-until python client.py; do
-    echo "'client.py' crashed with exit code $?. Restarting..." >&2
-    sleep 1
-done
--- client/test/client.py
+++
@@ -1,6 +0,0 @@
-import xbox_read
-
-for event in xbox_read.event_stream(deadzone=12000):
-    print " "
-    print event.key
-    print event.value
